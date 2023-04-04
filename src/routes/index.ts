16f88ab735a3e4a56422bec4d44fe8e8a4e87844
maxkrv <maxkr1203@gmail.com>
2023-04-04 21:35:31 +0300

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,7 @@ import categoryRouter from "./category.router";
 
 const router = Router();
 
-router.get("/", (_, res) => res.sendStatus(200));
+router.get("/", (_, res) => res.json({message: "success"}));
 router.use("/api", authRouter);
 router.use("/api", noteRouter);
 router.use("/api", todoRouter);
